164b366306c42bc2e9575e498adf7ac8ffc9eec7
ivan.panifedov <ivan.panifedov@gmail.com>
2023-01-25 07:58:50 -0500

    PostFilter,Select

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import PostList from './components/PostList';
 import PostForm from './components/UI/PostForm';
+import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css'
+import PostFilter from './components/PostFilter';
 
 function App() {
 
@@ -19,32 +22,39 @@ function App() {
       body: 'Descriptions',
     }
   ]); 
+  const [filter, setFilter] = useState({sort: '', query: ''});
+
+
+  const sortedPosts = useMemo(() => {
+    if(filter.sort) {
+      return [...posts].sort( (a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  }, [filter.sort, posts]);
 
   const createPost = (newPost) => {
     setPosts( [...posts, newPost])
-  }
+  };
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+ 
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <div>
-        <select>
-          <options value='value1' >123</options>
-        </select>
-      </div>
-      {posts.length !== 0
-        ?
-        <PostList remove={removePost} posts = {posts} title={' Посты JS '}/>
-        :
-        <h1 style={{textAlign: 'center'}}>
-          Посты не найдены!
-        </h1>
-      }
-      
+      <hr style={{margin: '15px'}} />
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
+        <PostList remove={removePost} posts = {sortedAndSearchedPosts} title={' Посты JS '}/>
+    
     </div>
   )
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import PostItem from './PostItem';
 
 function PostList({posts, title, remove}) {
+
+    if(!posts.length) {
+        return (
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены !
+            </h1>
+        )
+    }
+
     return (
         <div>
             <h1> {title}</h1>
