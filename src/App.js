26169067b4c8d7378b98144f9e2f47c2ee2169fc
ivan.panifedov <ivan.panifedov@gmail.com>
2023-01-25 12:52:08 -0500

    Fetching/arrayPosts/hook/utils

--- src/App.js
+++ src/App.js
@@ -11,28 +11,34 @@ import { usePosts } from './hooks/usePost';
 import axios from 'axios';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
 
   const [posts, setPosts] = useState([]); 
   const [filter, setFilter] = useState({sort: '', query: ''});
-  const[modal, setModal] = useState(false)
+  const[modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
+    let pagesArray = getPagesArray(totalPages);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching ( async() => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
+  })
+
+  
   useEffect(() => {
-    fetchPost()
+    fetchPosts()
   }, []);
 
-  async function fetchPost() {
-    setIsPostsLoading(true)
-    setTimeout( async() => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostsLoading(false)
-    }, 1000)
 
-  }
 
   const createPost = (newPost) => {
     setPosts( [...posts, newPost])
@@ -59,10 +65,16 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
+      {postError && 
+        <h1> Произошла ошибка ${postError}</h1>
+      }
       {isPostsLoading
         ? <div style={{display: 'flex', justifyContent: 'center' , marginTop: 200}}><Loader/></div>
         :<PostList remove={removePost} posts = {sortedAndSearchedPosts} title={' Посты JS '}/>
       }
+      {pagesArray.map(p => 
+        <MyButton>{p}</MyButton>  
+      )}
     
     </div>
   )
