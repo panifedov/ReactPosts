028dc08f6edd7a8d012b19c232df14e91499e916
ivan.panifedov <ivan.panifedov@gmail.com>
2023-01-25 09:06:25 -0500

    hooks, react-transition,

--- src/App.js
+++ src/App.js
@@ -5,50 +5,38 @@ import MySelect from './components/UI/select/MySelect';
 import MyInput from './components/UI/input/MyInput';
 import './styles/App.css'
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePost';
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id:1,
-      title: 'JavaScript',
-      body: 'Descriptions',
-    },
-    { id:2,
-      title: 'JavaScript 2 ',
-      body: 'Descriptions',
-    },
-    { id:3,
-      title: 'JavaScript 3 ',
-      body: 'Descriptions',
-    }
-  ]); 
+  const [posts, setPosts] = useState([]); 
   const [filter, setFilter] = useState({sort: '', query: ''});
+  const[modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-
-  const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts].sort( (a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
+  
   const createPost = (newPost) => {
     setPosts( [...posts, newPost])
+    setModal(false)
   };
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
- 
+
 
   return (
     <div className="App">
+      <MyButton
+        style={{marginTop: '15px'}}
+        onClick={() => setModal(true)}>
+        Создать Пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal} >
       <PostForm create={createPost}/>
-      <hr style={{margin: '15px'}} />
+      </MyModal>
       <PostFilter 
         filter={filter}
         setFilter={setFilter}
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PostItem from './PostItem';
 
 function PostList({posts, title, remove}) {
@@ -14,11 +15,17 @@ function PostList({posts, title, remove}) {
     return (
         <div>
             <h1> {title}</h1>
-            {posts.map(( post, index) =>
-                <div>
-                    <PostItem  remove={remove} number={index + 1} post={post} key={post.id} />
-                </div>
-            )}
+            <TransitionGroup>
+                {posts.map(( post, index) =>
+                    <CSSTransition
+                        key={post.id} 
+                        timeout={500}
+                        classNames='post'
+                    >
+                        <PostItem  remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 }
