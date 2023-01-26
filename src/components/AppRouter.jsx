f01060c45f3a1149c8e2fae14006448b36b48f25
ivan.panifedov <ivan.panifedov@gmail.com>
2023-01-26 17:49:33 -0500

    postId

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import About from '../pages/About';
 import Error from '../pages/Error';
+import PostIdPages from '../pages/PostIdPages';
 import Posts from '../pages/Posts';
 
 function AppRouter(props) {
     return (
         <Switch>
-            <Route patch='/posts'>
+            <Route exact patch='/posts'>
                 <Posts/>
             </Route>
             <Route patch='/about'>
                 <About/>
+            </Route>
+            <Route  exact patch='/post/:id'>
+                <PostIdPages/>
             </Route>
             <Route patch='/error'>
                 <Error/>
